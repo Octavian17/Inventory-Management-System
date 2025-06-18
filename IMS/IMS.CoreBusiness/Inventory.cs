2da51b586e32edf4b05b43586fc4f3c0b4188635
Octavian17 <prescura_octavian@yahoo.com>
2025-06-18 21:12:07 +0300

    added data annotation

--- IMS/IMS.CoreBusiness/Inventory.cs
+++ IMS/IMS.CoreBusiness/Inventory.cs
@@ -1,13 +1,19 @@
-﻿namespace IMS.CoreBusiness
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.CoreBusiness
 {
     public class Inventory
     {
         public int InventoryId { get; set; }
 
+        [Required]
+        [StringLength(150)]
         public string InventoryName { get; set; } = string.Empty;
 
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be a non-negative number.")]
         public int Quantity { get; set; }
 
+        [Range(0, int.MaxValue, ErrorMessage = "Price must be a non-negative number.")]
         public double Price { get; set; }
     }
 }
