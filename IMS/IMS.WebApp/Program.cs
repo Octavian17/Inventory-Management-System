7ed32f09f8aa327860c679b8faa3ca1fd352eb26
Octavian17 <prescura_octavian@yahoo.com>
2025-06-30 20:49:55 +0300

    added addProduct use case

--- IMS/IMS.WebApp/Program.cs
+++ IMS/IMS.WebApp/Program.cs
@@ -13,8 +13,8 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddSingleton<IInventoryRepository, InventoryRepository>();
+builder.Services.AddSingleton<IProductRepository, ProductRepository>();
 
- 
 
 builder.Services.AddTransient<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>();
 builder.Services.AddTransient<IAddInventoryUseCase, AddInventoryUseCase>();
@@ -24,6 +24,7 @@ builder.Services.AddTransient<IDeleteInventoryUseCase, DeleteInventoryUseCase>()
 
 builder.Services.AddTransient<IViewProductsByNameUseCase,ViewProductsByNameUseCase>();
 builder.Services.AddTransient<IDeleteProductUseCase, DeleteProductUseCase>();
+builder.Services.AddTransient<IAddProductUseCase, AddProductUseCase>();
 
 var app = builder.Build();
 
