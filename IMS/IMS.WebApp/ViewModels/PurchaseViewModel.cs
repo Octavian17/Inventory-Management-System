23270faced3e37f0bf01d5ff41d8f99ae16609ed
Octavian17 <prescura_octavian@yahoo.com>
2025-07-08 19:27:17 +0300

    finished PurchaseInventoryPage

---
+++ IMS/IMS.WebApp/ViewModels/PurchaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.ViewModels
+{
+    public class PurchaseViewModel
+    {
+        [Required]
+        public string PONumber { get; set; } = string.Empty;
+
+        [Range(minimum: 1, maximum:int.MaxValue, ErrorMessage ="You have to select an inventory")]
+        public int InventoryId { get; set; }
+
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Quantity has to be greater or equal to 1")]
+        public int QuantityToPurchase { get; set; }
+
+        [Range(minimum: 0.01, maximum: int.MaxValue, ErrorMessage = "Price has to be greater or equal to 0.01")]
+        public double InventoryPrice { get; set; }
+    }
+}
