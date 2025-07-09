8c2ae9b46204ba7c0976b30e4c44e4913dc2ca4b
Octavian17 <prescura_octavian@yahoo.com>
2025-07-09 20:41:38 +0300

    added ProduceProductUseCase

---
+++ IMS/IMS.UseCases/PluginInterfaces/IProductTransactionRepository.cs
@@ -0,0 +1,9 @@
+﻿using IMS.CoreBusiness;
+
+namespace IMS.UseCases.Activities
+{
+    public interface IProductTransactionRepository
+    {
+        Task ProduceAsync(string productionNumber, Product product, int quantity, string doneBy);
+    }
+}
