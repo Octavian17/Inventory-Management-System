42b1f7791790c8b6da3d31fb084d28923856a207
Octavian17 <prescura_octavian@yahoo.com>
2025-07-09 20:48:49 +0300

    added missing changes

--- IMS/IMS.CoreBusiness/ProductTransaction.cs
+++ IMS/IMS.CoreBusiness/ProductTransaction.cs
@@ -7,27 +7,27 @@ using System.Threading.Tasks;
 
 namespace IMS.CoreBusiness
 {
-	public class InventoryTransaction
+	public class ProductTransaction
 	{
-		public string PONumber { get; set; } = string.Empty;
+		public string SONumber { get; set; } = string.Empty;
 
         public string ProductionNumber { get; set; } = string.Empty;
 
-        public int InventoryTransactionId { get; set; }
+        public int ProductTransactionId { get; set; }
 
 		[Required]
-		public int InventoryId { get; set; }
+		public int ProductId { get; set; }
 
 		[Required]
 		public int QuantityBefore { get; set; }
 
 		[Required]
-		public InventoryTransactionType ActivityType { get; set; }
+		public ProductTransactionType ActivityType { get; set; }
 
 		[Required]
 		public int QuantityAfter { get; set; }
 
-		public double UnitPrice { get; set; }
+		public double? UnitPrice { get; set; }
 
 		[Required]
 		public DateTime TransactionDate { get; set; }
@@ -35,6 +35,6 @@ namespace IMS.CoreBusiness
 		[Required]
 		public string DoneBy { get; set; } = string.Empty;
 
-		public Inventory? Inventory { get; set; }
+		public Product? Product { get; set; }
 	}
 }
