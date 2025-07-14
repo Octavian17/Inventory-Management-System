42405f4555bf6c7b3883f362ba60c43d7e5d2bf0
Octavian17 <prescura_octavian@yahoo.com>
2025-07-14 21:18:49 +0300

    added custom validation attribute and javascript interop
    added javascript interop to not submit the edit form when the user press enter key

---
+++ IMS/IMS.WebApp/ViewModelsValidation/Produce_EnsureEnoughInventoryQuantity.cs
@@ -0,0 +1,30 @@
+﻿using IMS.WebApp.ViewModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.ViewModelsValidation
+{
+    public class Produce_EnsureEnoughInventoryQuantity: ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var produceViewModel = validationContext.ObjectInstance as ProduceViewModel;
+
+            if(produceViewModel != null)
+            {
+                if(produceViewModel.Product != null && produceViewModel.Product.ProductInventory != null)
+                {
+                    foreach(var productInventory in produceViewModel.Product.ProductInventory)
+                    {
+                        if(productInventory.Inventory != null &&
+                            productInventory.InventoryQuantity * produceViewModel.QuantityToProduce > productInventory.Inventory.Quantity)
+                        {
+                            return new ValidationResult($"The '{productInventory.Inventory.InventoryName}' does not have enough quantity to produce {produceViewModel.QuantityToProduce} of product '{produceViewModel.Product.ProductName}'.",
+                                new[] { validationContext.MemberName });
+                        }
+                    }
+                }    
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ IMS/IMS.WebApp/wwwroot/jsinterop.js
@@ -0,0 +1,8 @@
+﻿function proventFormSubmission(formId) {
+    document.getElementById(formId).addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            return false;
+        }
+    });
+}
