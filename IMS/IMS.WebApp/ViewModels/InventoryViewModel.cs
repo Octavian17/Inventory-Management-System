f9f5d082dca7f396268f413756f5900a7885ab5c
OctavianPrescura <octavian.prescura@trimble.com>
2025-08-20 15:00:28 +0300

    solve blazor ssr errors

---
+++ IMS/IMS.WebApp/ViewModels/InventoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.CoreBusiness
+{
+    public class InventoryViewModel
+    {
+        public int InventoryId { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string InventoryName { get; set; } = string.Empty;
+
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be a non-negative number.")]
+        public int Quantity { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Price must be a non-negative number.")]
+        public double Price { get; set; }
+	}
+}
